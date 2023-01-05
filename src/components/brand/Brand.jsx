22e9ffe926b4fbd176adb39765988fa7fcc4c6e3
Ugwuanyi Love Ekene <49119675+Hubnotch@users.noreply.github.com>
2023-01-05 01:15:24 +0100

    Added styles for the brands

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import './brand.css'
-const Brand = () => {
-  return (
-    <div>Brand</div>
-  )
-}
+import React from 'react';
+import { google, slack, atlassian, dropbox, shopify } from './import';
+import './brand.css';
 
-export default Brand
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
+    <div>
+      <img src={google} alt='brands logo'/>
+    </div>
+    <div>
+      <img src={slack} alt='brands logo'/>
+    </div>
+    <div>
+      <img src={atlassian} alt='brands logo'/>
+    </div>
+    <div>
+      <img src={dropbox} alt='brands logo'/>
+    </div>
+    <div>
+      <img src={shopify} alt='brands logo'/>
+    </div>
+  </div>
+);
+
+export default Brand;
